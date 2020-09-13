a5d28ce18f1752503028374a0d252d5e45cb68a0
cseas <contact.abhijeetsingh@gmail.com>
2020-09-13 11:06:22 +0530

    Updates

---
+++ precache-manifest.3b34bd791537a12db1c3f77160bdf019.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "306b3174cb9dbb0d24a01516c08009c1",
+    "url": "/react-swiper/index.html"
+  },
+  {
+    "revision": "bc95709e828f1f329487",
+    "url": "/react-swiper/static/css/2.031ca112.chunk.css"
+  },
+  {
+    "revision": "8845c55f14684b36cd2a",
+    "url": "/react-swiper/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bc95709e828f1f329487",
+    "url": "/react-swiper/static/js/2.c6d19cf4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-swiper/static/js/2.c6d19cf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8845c55f14684b36cd2a",
+    "url": "/react-swiper/static/js/main.b9c4a12d.chunk.js"
+  },
+  {
+    "revision": "771b5562f33e11a2f6e2",
+    "url": "/react-swiper/static/js/runtime-main.9f54978a.js"
+  }
+]);
